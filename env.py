c9422ecebe30cf858a98b548f7ddc2a8cd5a2b2d
Tielor <1819447603@qq.com>
2024-08-03 16:59:16 +0800

    add the load

--- env.py
+++ env.py
@@ -10,11 +10,14 @@ gym.logger.set_level(40)
 
 class TZBEnv(gym.Env):
     def __init__(self):
-        self.state = np.zeros(12) #状态空间
+        self.state = np.zeros(12)
+        # self.state = np.array([118.2, 25.8, 10000, 118.4, 25.8, 10000, 118.6, 25.8, 10000, 118.8, 25.8, 10000]) #状态空间
         self.seed()
+        # state_high = np.array([120, 26, 10000, 120, 26, 10000, 120, 26, 10000, 120, 26, 10000])
+        # state_low = np.array([118, 23, 10000, 118, 23, 10000, 118, 23, 10000, 118, 23, 10000])
 
         self.observation_space = spaces.Box(low=-np.inf, high=np.inf, shape=(12, ), dtype=np.float)
-        self.action_space = spaces.Box(low=118, high=119, shape=(12, ), dtype=np.float)
+        self.action_space = spaces.Box(low=118, high=120, shape=(12, ), dtype=np.float)
         self.rew_space = spaces.Box(low=-np.inf, high=np.inf, shape=(1, ), dtype=np.float)
         # print("debug:", self.action_space.shape)       
 
--- generate_data.py
+++ generate_data.py
@@ -6,12 +6,23 @@ import random
 def generate_info(seeds):
     random.seed(seeds)
     height = 10000
-    p1 = [round(random.uniform(118.0, 119.0), 2), round(random.uniform(25.0, 26.0), 2), height]
-    p2 = [round(random.uniform(118.0, 119.0), 2), round(random.uniform(25.0, 26.0), 2), height]
-    p3 = [round(random.uniform(118.0, 119.0), 2), round(random.uniform(25.0, 26.0), 2), height]
-    p4 = [round(random.uniform(118.0, 119.0), 2), round(random.uniform(25.0, 26.0), 2), height]
-    kill = random.randint(0, 4)
+    p1 = [round(random.uniform(118.45, 118.55), 2), round(random.uniform(25.15, 25.25), 2), height]
+    p2 = [round(random.uniform(118.65, 118.75), 2), round(random.uniform(24.95, 25.05), 2), height]
+    p3 = [round(random.uniform(118.85, 118.95), 2), round(random.uniform(25.15, 25.25), 2), height]
+    p4 = [round(random.uniform(119.05, 119.15), 2), round(random.uniform(24.95, 25.05), 2), height]
+    kill = random.randint(0, 3)
     death = random.randint(0, 4)
+
+    return p1, p2, p3, p4, kill, death
+
+def generate_info_good():
+    height = 10000
+    p1 = [118.5, 25.2, height]
+    p2 = [118.7, 25.0, height]
+    p3 = [118.9, 25.2, height]
+    p4 = [119.1, 25.0, height]
+    kill = 4
+    death = 0
 
     return p1, p2, p3, p4, kill, death
     
@@ -21,8 +32,11 @@ data_dict = {}
 info = {}
 
 # 添加每条数据到字典中
-for i in range(1, 1101):
-    p1, p2, p3, p4, k, d = generate_info(i)
+for i in range(1, 1500):
+    if i % 17 == 0:
+        p1, p2, p3, p4, k, d = generate_info_good()
+    else:
+        p1, p2, p3, p4, k, d = generate_info(i)
     data_dict["100000"] = p1
     data_dict["200000"] = p2
     data_dict["300000"] = p3
